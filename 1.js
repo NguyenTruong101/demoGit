8f1d296d1fcb7f6aa585e0ca537550ae5a3482fc
Nguyễn Duy Trường <96111971+NguyenTruong101@users.noreply.github.com>
2022-09-12 19:44:54 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,6 @@
+export let last = "DuyTruong";
+export let  first = "Hello";
+
+export function L1(){
+    return first +  last;
+}
---
+++ app.js
@@ -0,0 +1,8 @@
+import { L1 } from './1.js'
+import {first,last} from './1.js'
+
+
+
+
+
+console.log("All = " + L1());
